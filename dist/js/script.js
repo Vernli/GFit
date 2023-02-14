1a4bf24411d34a6c3941bb4c0d54aa8bb1e49391
Dawid Krajewski <dawidkrajewski@MacBook-Air-Dawid.local>
2023-02-14 09:24:40 +0100

    Zmiana video na mobilkach

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,10 +1,72 @@
 const btn = document.getElementById("menu-btn");
 const menu = document.getElementById("menu");
 
+const video_content = document.querySelectorAll(".video-content");
+const videos = document.querySelectorAll(".video");
+
+const button_left = document.getElementById("left");
+const button_right = document.getElementById("right");
+let current_video = 0;
+
 btn.addEventListener("click", navToggle);
+
+button_left.addEventListener("click", videoToggleL);
+button_right.addEventListener("click", videoToggleR);
+
+function videoToggleL() {
+  if (current_video == 0) {
+    // Hide current
+    videos[current_video].classList.toggle("hidden");
+    current_video = 3;
+    // Show next
+    videos[current_video].classList.toggle("hidden");
+  } else if (current_video == 3) {
+    videos[current_video].classList.toggle("hidden");
+    current_video = 2;
+    videos[current_video].classList.toggle("hidden");
+  } else if (current_video == 2) {
+    videos[current_video].classList.toggle("hidden");
+    current_video = 1;
+    videos[current_video].classList.toggle("hidden");
+  } else if (current_video == 1) {
+    videos[current_video].classList.toggle("hidden");
+    current_video = 0;
+    videos[current_video].classList.toggle("hidden");
+  }
+}
+
+function videoToggleR() {
+  if (current_video == 0) {
+    // Hide current
+    videos[current_video].classList.toggle("hidden");
+    current_video = 1;
+    // Show next
+    videos[current_video].classList.toggle("hidden");
+  } else if (current_video == 1) {
+    videos[current_video].classList.toggle("hidden");
+    current_video = 2;
+    videos[current_video].classList.toggle("hidden");
+  } else if (current_video == 2) {
+    videos[current_video].classList.toggle("hidden");
+    current_video = 3;
+    videos[current_video].classList.toggle("hidden");
+  } else if (current_video == 3) {
+    videos[current_video].classList.toggle("hidden");
+    current_video = 0;
+    videos[current_video].classList.toggle("hidden");
+  }
+}
 
 function navToggle() {
   btn.classList.toggle("open");
   menu.classList.toggle("flex");
   menu.classList.toggle("hidden");
+}
+
+if (window.innerWidth < 780) {
+  video_content.forEach((element) => {
+    element.classList.remove("hover:opacity-100");
+    element.classList.remove("opacity-0");
+    element.classList.add("opacity-100");
+  });
 }
