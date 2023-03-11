875c7aafb6752dd021594b2698897bfe1538eac9
Dawid Krajewski <dawidkrajewski@MacBook-Air-Dawid.local>
2023-03-11 11:32:36 +0100

    dynamiczne dodawanie klas

--- dist/js/script.js
+++ dist/js/script.js
@@ -8,33 +8,7 @@ const button_left = document.getElementById("left");
 const button_right = document.getElementById("right");
 let current_video = 0;
 
-btn.addEventListener("click", navToggle);
-
-button_left.addEventListener("click", videoToggleL);
-button_right.addEventListener("click", videoToggleR);
-
-function videoToggleL() {
-  if (current_video == 0) {
-    videos[current_video].classList.toggle("hidden");
-    current_video = 3;
-    videos[current_video].classList.toggle("hidden");
-  } else if (current_video == 3) {
-    videos[current_video].classList.toggle("hidden");
-    current_video = 2;
-    videos[current_video].classList.toggle("hidden");
-  } else if (current_video == 2) {
-    videos[current_video].classList.toggle("hidden");
-    current_video = 1;
-    videos[current_video].classList.toggle("hidden");
-  } else if (current_video == 1) {
-    videos[current_video].classList.toggle("hidden");
-    current_video = 0;
-    videos[current_video].classList.toggle("hidden");
-  }
-}
-
 function videoToggleR() {
-  console.log(current_video);
   if (current_video == 0) {
     // Hide current
     videos[current_video].classList.toggle("hidden");
@@ -56,18 +30,49 @@ function videoToggleR() {
   }
 }
 
+function videoToggleL() {
+  if (current_video == 0) {
+    videos[current_video].classList.toggle("hidden");
+    current_video = 3;
+    videos[current_video].classList.toggle("hidden");
+  } else if (current_video == 3) {
+    videos[current_video].classList.toggle("hidden");
+    current_video = 2;
+    videos[current_video].classList.toggle("hidden");
+  } else if (current_video == 2) {
+    videos[current_video].classList.toggle("hidden");
+    current_video = 1;
+    videos[current_video].classList.toggle("hidden");
+  } else if (current_video == 1) {
+    videos[current_video].classList.toggle("hidden");
+    current_video = 0;
+    videos[current_video].classList.toggle("hidden");
+  }
+}
+
 function navToggle() {
   btn.classList.toggle("open");
   menu.classList.toggle("flex");
   menu.classList.toggle("hidden");
 }
 
-if (window.innerWidth < 780) {
-  video_content.forEach((element) => {
-    element.classList.remove("hover:opacity-100");
-    element.classList.remove("opacity-0");
-    element.classList.add("opacity-100");
-  });
-}
+btn.addEventListener("click", navToggle);
 
-console.log(videos);
+button_left.addEventListener("click", videoToggleL);
+button_right.addEventListener("click", videoToggleR);
+
+window.addEventListener("resize", (e) => {
+  if (e.target.innerWidth < 780 || e.target.innerHeight < 440) {
+    video_content.forEach((element) => {
+      element.classList.remove("hover:opacity-100");
+      element.classList.remove("opacity-0");
+      element.classList.add("opacity-100");
+    });
+  } else {
+    video_content.forEach((element) => {
+      element.classList.add("hover:opacity-100");
+      element.classList.add("opacity-0");
+      element.classList.remove("opacity-100");
+    });
+  }
+});
