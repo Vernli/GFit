ba7623334ffb16eee77d6a2f96f9b33eae82d703
Vernli <dawid_krajewski@outlook.com>
2023-04-18 16:52:09 +0200

    function videToogle[R/L] - change to function shwoVideo

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,98 +1,54 @@
-const menuBtn = document.getElementById("menu-btn");
-const heroBtn = document.querySelectorAll(".btn-hero");
+const menu_btn = document.getElementById("menu-btn");
+const hero_btn = document.querySelectorAll(".btn-hero");
 const menu = document.getElementById("menu");
 
 const videocontent = document.querySelectorAll(".video-content");
 const videos = document.querySelectorAll(".video");
 
-const button_left = document.getElementById("left");
-const button_right = document.getElementById("right");
+const button_left = document.getElementById("prev");
+const button_right = document.getElementById("next");
 let current_video = 0;
 
-heroBtn.forEach((btn) => {
+hero_btn.forEach((btn) => {
   btn.addEventListener("click", {});
 });
 
 // Mobile
-function videoToggleR(e) {
+function showVideo(e) {
   const direction = e.target.id;
-  console.log(current_video);
   switch (current_video) {
     case 0:
       videos[current_video].classList.toggle("hidden");
-      direction == "right" ? (current_video = 1) : (current_video = 3);
+      direction === "next" ? (current_video = 1) : (current_video = 3);
       videos[current_video].classList.toggle("hidden");
       break;
     case 1:
       videos[current_video].classList.toggle("hidden");
-      current_video = 2;
+      direction === "next" ? (current_video = 2) : (current_video = 0);
       videos[current_video].classList.toggle("hidden");
       break;
     case 2:
       videos[current_video].classList.toggle("hidden");
-      direction == "right" ? (current_video = 1) : (current_video = 3);
-
+      direction === "next" ? (current_video = 3) : (current_video = 1);
       videos[current_video].classList.toggle("hidden");
       break;
     case 3:
       videos[current_video].classList.toggle("hidden");
-      current_video = 0;
+      direction === "next" ? (current_video = 0) : (current_video = 2);
       videos[current_video].classList.toggle("hidden");
   }
 }
 
-// function videoToggleL() {
-//   console.log(current_video);
-//   switch (current_video) {
-//     case 0:
-//       videos[current_video].classList.toggle("hidden");
-//       current_video = 3;
-//       videos[current_video].classList.toggle("hidden");
-//       break;
-//     case 1:
-//       videos[current_video].classList.toggle("hidden");
-//       current_video = 2;
-//       videos[current_video].classList.toggle("hidden");
-//       break;
-//     case 2:
-//       videos[current_video].classList.toggle("hidden");
-//       current_video = 1;
-//       videos[current_video].classList.toggle("hidden");
-//       break;
-//     case 3:
-//       videos[current_video].classList.toggle("hidden");
-//       current_video = 0;
-//       videos[current_video].classList.toggle("hidden");
-//   }
-// if (current_video == 0) {
-//   videos[current_video].classList.toggle("hidden");
-//   current_video = 3;
-//   videos[current_video].classList.toggle("hidden");
-// } else if (current_video == 3) {
-//   videos[current_video].classList.toggle("hidden");
-//   current_video = 2;
-//   videos[current_video].classList.toggle("hidden");
-// } else if (current_video == 2) {
-//   videos[current_video].classList.toggle("hidden");
-//   current_video = 1;
-//   videos[current_video].classList.toggle("hidden");
-// } else if (current_video == 1) {
-//   videos[current_video].classList.toggle("hidden");
-//   current_video = 0;
-//   videos[current_video].classList.toggle("hidden");
-// }
-// }
-
 function navToggle() {
-  menuBtn.classList.toggle("open");
+  menu_btn.classList.toggle("open");
   menu.classList.toggle("flex");
   menu.classList.toggle("hidden");
 }
 
 function Init() {
-  menuBtn.addEventListener("click", navToggle);
-  button_left.addEventListener("click", videoToggleR);
-  button_right.addEventListener("click", videoToggleR);
+  menu_btn.addEventListener("click", navToggle);
+  button_left.addEventListener("click", showVideo);
+  button_right.addEventListener("click", showVideo);
 }
 
 Init();
