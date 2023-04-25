ef1e71f77d146c3bc15fe7641f7917f36a45f296
Vernli <dawid_krajewski@outlook.com>
2023-04-25 21:34:57 +0200

    calendar

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,17 +1,34 @@
+// Navbar
 const menu_btn = document.getElementById("menu-btn");
 const hero_btn = document.querySelectorAll(".btn-hero");
 const menu = document.getElementById("menu");
 
+// Video
 const videocontent = document.querySelectorAll(".video-content");
 const videos = document.querySelectorAll(".video");
-
 const button_left = document.getElementById("prev");
 const button_right = document.getElementById("next");
 let current_video = 0;
 
-hero_btn.forEach((btn) => {
-  btn.addEventListener("click", {});
-});
+// Calendar
+const container = document.querySelector(".calendar-container");
+const dateInput = document.querySelectorAll("input[type='date']");
+const todayDate = new Date();
+const date = new Date();
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 // Mobile
 function showVideo(e) {
@@ -53,29 +70,111 @@ function InitMobile() {
 InitMobile();
 
 // Calendar
-const date = new Date();
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-
-const dateInput = document.querySelectorAll("input[type='date']");
 function setTodayDate() {
   dateInput.forEach((input) => {
-    input.value = `${date.getFullYear()}-${
-      date.getMonth() <= 9 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1
-    }-${date.getDate() <= 9 ? "0" + date.Date() : date.getDate()}`;
+    input.value = `${todayDate.getFullYear()}-${
+      todayDate.getMonth() <= 9
+        ? "0" + (todayDate.getMonth() + 1)
+        : todayDate.getMonth() + 1
+    }-${
+      todayDate.getDate() <= 9 ? "0" + todayDate.getDate() : todayDate.getDate()
+    }`;
   });
 }
-window.addEventListener("DOMContentLoaded", setTodayDate);
-function renderCalendar() {}
+
+//
+const renderCalendar = () => {
+  date.setDate(1);
+
+  const monthDays = document.querySelector(".days");
+
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
+
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
+
+  const firstDayIndex = date.getDay();
+
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
+
+  const nextDays = 7 - lastDayIndex - 1;
+
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+
+  document.querySelector(".date p").innerHTML = new Date().toDateString();
+
+  let days = "";
+
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDay; i++) {
+    if (
+      i === new Date().getDate() &&
+      date.getMonth() === new Date().getMonth()
+    ) {
+      days += `<div class="today">${i}</div>`;
+    } else {
+      days += `<div>${i}</div>`;
+    }
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
+    monthDays.innerHTML = days;
+  }
+};
+
+document.querySelector(".prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+});
+
+document.querySelector(".days").addEventListener("click", (e) => {
+  let month = document.querySelector(".date h1").innerText.toLowerCase();
+  if (e.target.classList.contains("prev-date")) {
+    month = months
+      .indexOf(month.charAt(0).toUpperCase() + month.slice(1))
+      .toString();
+    dateInput[0].value = `${date.getFullYear()}-${
+      month.length <= 1 ? "0" + month : month
+    }-${e.target.innerText}`;
+  } else if (e.target.classList.contains("next-date")) {
+    month = (
+      months.indexOf(month.charAt(0).toUpperCase() + month.slice(1)) + 2
+    ).toString();
+    dateInput[0].value = `${date.getFullYear()}-${
+      month.length <= 1 ? "0" + month : month
+    }-${"0" + e.target.innerText}`;
+  } else {
+    month = (
+      months.indexOf(month.charAt(0).toUpperCase() + month.slice(1)) + 1
+    ).toString();
+    dateInput[0].value = `${date.getFullYear()}-${
+      month.length <= 1 ? "0" + month : month
+    }-${
+      e.target.innerText.length <= 1
+        ? "0" + e.target.innerText
+        : e.target.innerText
+    }`;
+  }
+});
+setTodayDate();
+renderCalendar();
